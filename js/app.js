ba8f395ad11d3e27fca578b55914d5f9259033c5
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-05-15 03:00:25 +0200

    Change - refactor, validate answer when clicking 'Next' btn, require correct answer, better responsive & styling, v-cloak loading, etc...

--- js/app.js
+++ js/app.js
@@ -1,51 +1,81 @@
-    window.onload=function(){
-      // Create a quiz object with a title and two questions.
-      // A question has one or more answer, and one or more is valid.
-      var quiz = {
-        title: 'Quiz',
-        questions: [
-          {
-            title: "Question 1",
-            text: "Placeholder Text",
-            responses: [
-              {text: 'Wrong, too bad.'},
-              {text: 'Right!', correct: true},
-            ]
-          }, {
-            title: "Question 2",
-            text: "",
-            responses: [
-              {text: 'Right answer', correct: true},
-              {text: 'Wrong answer'},
-            ]
-          }
-        ]
-      };
+        window.onload=function() {
 
-      new Vue({
-        el: '#quiz',
+        // Quiz Data
+        var quiz = {
+            title: 'Quiz',
+            questions: [
+                {
+                    title: "Should you always wear Grundfos-provided Helmets?",
+                    description: "Helmets to wear on your head during the tour.",
+                    answers: [
+                        { text: 'Yes, but you can bring your own.', correct: false, message: 'Incorrect. You must bring your own hat.' },
+                        { text: 'Yes.', correct: true, message: 'Correct! Helmets must be worn at all times.' },
+                        { text: 'No.', correct: false, message: 'Wrong. You must wear helmets!' },
+                    ],
+                    message: '',
+                    answered: false,
+                    correct: false,
+                }, {
+                    title: "Question 2",
+                    description: "",
+                    answers: [
+                        { text: 'Wrong, too bad.', correct: false, message: 'Sorry, that\'s incorrect!' },
+                        { text: 'Right!', correct: true, message: 'Awesome, that\'s correct!' },
+                        { text: 'Wrong!', correct: false, message: 'Wrong. You must wear protection hats!' },
+                    ],
+                    message: '',
+                    answered: false,
+                    correct: false,
+                }
+            ]
+        };
+
+    // Application
+    new Vue({
+        el: "#app",
         data: {
-          quiz: quiz,
-          // Store current question index
-          questionIndex: 0,
-          // An array initialized with "false" values for each question
-          // It means: "did the user answered correctly to the question n?" "no".
-          userResponses: Array(quiz.questions.length).fill(false)
+            // slides: slides,
+            // slideIndex: 0,
+            quiz: quiz,
+            questionIndex: 0,
+            quizCompleted: false,
+            questionCompleted: false,
+            responses: new Array(quiz.questions.length)
         },
-        // The view will trigger these methods on click
         methods: {
-          // Go to next question
-          next: function() {
-            this.questionIndex++;
-          },
-          // Go to previous question
-          prev: function() {
-            this.questionIndex--;
-          },
-          // Return "true" count in userResponses
-          score: function() {
-            return this.userResponses.filter(function(val) { return val }).length;
-          }
+            next: function() {
+
+                // Get question and answer
+                var question = this.quiz.questions[this.questionIndex];
+                var answer = question.answers[this.responses[this.questionIndex]];
+
+                // Set data
+                question.answered = true;
+                question.correct = answer.correct;
+                question.message = answer.message;
+
+                //  If answer correct, proceed
+                if (question.correct === true) {
+                    this.questionIndex++;
+                }
+            },
+            prev: function() {
+
+                // Reset answered if answer was incorrect.
+                var question = this.quiz.questions[this.questionIndex];
+                if (question.answered && question.correct === false) {
+                    question.answered = false;
+                }
+
+                this.questionIndex--;
+            },
+            unanswer: function () {
+                this.quiz.questions[this.questionIndex].answered = false;
+            },
+            // TODO: Refactor
+            score: function () {
+                return quiz.questions.length;
+            }
         }
-      });
-    }
+    });
+}
