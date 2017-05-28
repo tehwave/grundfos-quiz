0c2bde6b46aaebcf54296a2735ab40079319332b
Peter C. Jørgensen <peterchrjoergensen@gmail.com>
2017-05-28 20:00:02 +0200

    Add - guideslines, recommendations, user, slides

--- js/app.js
+++ js/app.js
@@ -1,41 +1,67 @@
-        window.onload=function() {
+    window.onload=function() {
 
-        // Quiz Data
-        var quiz = {
-            title: 'Quiz',
-            questions: [
-                {
-                    title: "Should you always wear Grundfos-provided Helmets?",
-                    description: "Helmets to wear on your head during the tour.",
-                    answers: [
-                        { text: 'Yes, but you can bring your own.', correct: false, message: 'Incorrect. You must bring your own hat.' },
-                        { text: 'Yes.', correct: true, message: 'Correct! Helmets must be worn at all times.' },
-                        { text: 'No.', correct: false, message: 'Wrong. You must wear helmets!' },
-                    ],
-                    message: '',
-                    answered: false,
-                    correct: false,
-                }, {
-                    title: "Question 2",
-                    description: "",
-                    answers: [
-                        { text: 'Wrong, too bad.', correct: false, message: 'Sorry, that\'s incorrect!' },
-                        { text: 'Right!', correct: true, message: 'Awesome, that\'s correct!' },
-                        { text: 'Wrong!', correct: false, message: 'Wrong. You must wear protection hats!' },
-                    ],
-                    message: '',
-                    answered: false,
-                    correct: false,
-                }
-            ]
-        };
+    var user = {
+        language: '',
+        name: '',
+        company: ''
+    };
 
-    // Application
+    var guidelines = [
+        {
+            image: 'evacuation.png',
+            text: 'Evakuation'
+        },
+        {
+            image: 'vest.png',
+            text: 'Vest'
+        }
+    ];
+
+    var recommendations = [
+        {
+            image: 'stairs.png',
+            text: 'Forsigtig færdsel på trapper. Vi anbefaler, at man holder ved gelænder ved færdsel på trapper.'
+        },
+    ];
+
+    var quiz = {
+        title: 'Quiz',
+        questions: [
+            {
+                title: "Ved rundgangen skal jeg?",
+                description: "",
+                image: '',
+                answers: [
+                    { text: 'Altid blive sammen med gruppen', correct: true },
+                    { text: 'Det er OK, hvis jeg stopper, hvis noget ser interessant ud', correct: false },
+                    { text: 'Hvis jeg bare følger op senere, er det OK', correct: false },
+                ],
+                message: '',
+                answered: false,
+                correct: false,
+            }, {
+                title: "Question 2",
+                description: "Description",
+                image: 'question2.png',
+                answers: [
+                    { text: 'Wrong, too bad.', correct: false },
+                    { text: 'Right!', correct: true },
+                    { text: 'Wrong!', correct: false },
+                ],
+                message: '',
+                answered: false,
+                correct: false,
+            }
+        ]
+    };
+
     new Vue({
         el: "#app",
         data: {
-            // slides: slides,
-            // slideIndex: 0,
+            slideIndex: 2,
+            user: user,
+            guidelines: guidelines,
+            recommendations: recommendations,
             quiz: quiz,
             questionIndex: 0,
             quizCompleted: false,
@@ -43,8 +69,17 @@
             responses: new Array(quiz.questions.length)
         },
         methods: {
+            selectLanguage: function(language) {
+                this.user.language = language;
+                this.nextSlide();
+            },
+            nextSlide: function () {
+                this.slideIndex++;
+            },
+            prevSlide: function () {
+                this.slideIndex--;
+            },
             next: function() {
-
                 // Get question and answer
                 var question = this.quiz.questions[this.questionIndex];
                 var answer = question.answers[this.responses[this.questionIndex]];
@@ -52,14 +87,20 @@
                 // Set data
                 question.answered = true;
                 question.correct = answer.correct;
-                question.message = answer.message;
 
                 //  If answer correct, proceed
                 if (question.correct === true) {
                     this.questionIndex++;
+                } else {
+                    question.message = 'Wrong.';
                 }
             },
             prev: function() {
+
+                if (this.questionIndex === 0) {
+                    this.prevSlide();
+                    return;
+                }
 
                 // Reset answered if answer was incorrect.
                 var question = this.quiz.questions[this.questionIndex];
